6244f206cc6bb60c99c4244084c64e0a73fefcb8
AkaElisee <akajeanelisee@gmail.com>
2020-02-21 17:33:14 +0100

    Architecture DevBody

--- src/Components/DevBody.js
+++ src/Components/DevBody.js
@@ -3,8 +3,37 @@ import React, { Component } from 'react'
 export default class DevBody extends Component {
     render() {
         return (
-            <div>
-                
+            <div className = "dev">
+                <div className = "">
+                    <div className = "button-dev">
+                        <button> Voici WeDev </button>
+                    </div>
+                    <div className = "title-dev">
+                        <p> APPLICATION DE GESTION</p>
+                        <p> DE PROEJET POUR LES DEV</p>
+                    </div>
+                    <div className ="text-dev">
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem error commodi nostrum veniam. Explicabo minima id illum reprehenderit ipsam autem aspernatur natus officiis sit error, voluptatibus hic ab qui atque.</p>
+                    </div>
+                </div>
+                <div className = "dev__item">
+                    <div>
+                        <i></i>
+                        <p>Vos reporting centralisés</p>
+                    </div>
+                    <div>
+                        <i></i>
+                        <p>Le service de nous activité en temps réel</p>
+                    </div>
+                    <div>
+                        <i></i> 
+                        <p>Un gain de temps pour le pilotage</p>
+                    </div>
+                    <div>
+                        <i></i>
+                        <p>Des utilitaires pour votre dev</p>
+                    </div>
+                </div>
             </div>
         )
     }
