b8340beb176fef20e6846d7e5d9a3d53f8679553
AkaElisee <akajeanelisee@gmail.com>
2020-03-27 09:28:51 +0100

    ajout des routes projects myprofil customer

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,12 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Inscription from './pages/Inscription';
+import Myprofil from './pages/Myprofil';
+import Customer from './pages/Customer/Customer';
+import Project from './pages/Projects/Project';
+import AddCustomer from './pages/Customer/AddCustomer'
+import EditCustomer from './pages/Customer/EditCustomer';
+import AddProject from './pages/Projects/AddProject';
 
 
 export class Routes extends Component {
@@ -14,6 +20,12 @@ export class Routes extends Component {
                 <Route exact path = "/login" component = { Login } />
                 <Route exact path = "/inscription" component = { Inscription } />
                 <Route exact path = "/dashboard" component = { Dashboard } />
+                <Route exact path = "/profil" component = { Myprofil } />
+                <Route exact path = "/client" component = { Customer } /> 
+                <Route exact path = "/client/Add" component = { AddCustomer } />
+                <Route exact path = "/client/edit" component = { EditCustomer } />
+                <Route exact path = "/projet" component = { Project } />
+                <Route exact path = "/projet/add" component = { AddProject } />
             </Fragment>
         )
     }
