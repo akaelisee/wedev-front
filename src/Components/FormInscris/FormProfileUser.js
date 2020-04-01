6365f9c408ac5713c910c815448b23c480031cf4
jotruong-ynov <jonathan.truong@ynov.com>
2020-04-01 22:30:59 +0200

    formProfileUser

--- src/Components/FormInscris/FormProfileUser.js
+++ src/Components/FormInscris/FormProfileUser.js
@@ -2,11 +2,53 @@ import React, { Component } from 'react'
 
 export default class FormProfileUser extends Component {
     
+    constructor(props) {
+        super(props)
+        this.state = {
+            list_status : [],
+            list_profil: [],
+            test: ""
+        }
+    }
+
     continue = e => {
         e.preventDefault()
         this.props.nextForm()
     }
     
+    getProfil() {
+        fetch("https://vast-headland-40106.herokuapp.com/api/user_profile")
+            .then(res => res.json())
+            .then(arr => {
+                let new_tab = [];
+                arr.data.map((item) => {
+                    new_tab.push(item.label)
+                })
+            this.setState({list_profil: new_tab})
+        })
+            .catch(err => console.error('ERROR: ' , err))
+    }
+
+    getStatus(){
+        fetch("https://vast-headland-40106.herokuapp.com/api/corporate")
+            .then(res => res.json())
+            .then(arr => {
+                let new_tab = [];
+                arr.data.map((item) => {
+                    new_tab.push(item.label)
+                })
+                this.setState({list_status: new_tab})
+        })
+            .catch(err => console.error('ERROR: ' , err))
+    }
+
+    
+    componentDidMount(){
+        //fetch to get list status and profil
+        this.getStatus()
+        this.getProfil()
+    }
+
     render() {
         const {
                 handleChange, 
@@ -18,6 +60,13 @@ export default class FormProfileUser extends Component {
                 statut,
                 profil
                 } = this.props;
+        const all_status = this.state.list_status.map((label) => {
+        return <option>{label}</option>
+        })
+
+        const all_profil = this.state.list_profil.map((label) =>{
+            return <option>{label}</option>
+        }) 
         return (
             <>
                 <div className="container-inscris">
@@ -83,22 +132,24 @@ export default class FormProfileUser extends Component {
                         <div className="form-select">
                             <div className="box-select">
                                 <select value = {statut}>
-                                    <option value = "SAS"> Status Société </option> 
+                                    {/* <option value = "SAS"> Status Société </option> 
                                     <option value = "SAS"> SAS</option> 
                                     <option value = "SASU"> SASU</option> 
                                     <option value = "Auto-entrepreneur"> Auto-entrepreneur</option> 
                                     <option value = "EURL"> EURL</option> 
-                                    <option value = "SARL"> SARL</option> 
+                                    <option value = "SARL"> SARL</option>  */}
+                                    {all_status}
                                 </select>
                             </div>
                             <div className="box-select box">
                                 <select value = {profil}>
-                                    <option value = "back"> Votre profil</option> 
+                                    {/* <option value = "back"> Votre profil</option> 
                                     <option value = "back"> Développeur Back</option> 
                                     <option value = "front"> Développeur Front</option> 
                                     <option value = "data"> Big data</option> 
                                     <option value = "analyst"> Analyst</option> 
-                                    <option value = "qa"> QA</option> 
+                                    <option value = "qa"> QA</option>  */}
+                                    {all_profil}
                                 </select>
                             </div>
                         </div>
